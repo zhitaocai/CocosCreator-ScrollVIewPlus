a7f77f664b929d625958a45b1cbd3518d0f6461a
caizhitao <czt.saisam@gmail.com>
2019-06-24 11:58:10 +0800

    add comment

--- assets/01/scripts/ScrollViewCtrl.ts
+++ assets/01/scripts/ScrollViewCtrl.ts
@@ -22,10 +22,10 @@ export default class DirectLoadScrollViewCtrl extends cc.Component {
 		itemNode.getComponent(ItemPrefabCtrl).bindData(itemIndex);
 	}
 
-	async directLoad(childNodeCount: number) {
+	async directLoad(length: number) {
 		await new Promise((resolve, reject) => {
 			this.scrollView.content.removeAllChildren();
-			for (let i = 0; i < childNodeCount; i++) {
+			for (let i = 0; i < length; i++) {
 				this._initItem(i);
 			}
 			resolve();
@@ -49,14 +49,22 @@ export default class DirectLoadScrollViewCtrl extends cc.Component {
 	private executePreFrame(generator: Generator, duration: number) {
 		return new Promise((resolve, reject) => {
 			let gen = generator;
+			// 创建执行函数
 			let execute = () => {
+				// 执行之前，先记录开始时间
 				let startTime = new Date().getTime();
+
+				// 然后一直从 Generator 中获取已经拆分好的代码段出来执行
 				for (let iter = gen.next(); ; iter = gen.next()) {
+					// 判断是否已经执行完所有 Generator 的小代码段，如果是的话，那么就表示任务完成
 					if (iter == null || iter.done) {
 						resolve();
 						return;
 					}
+
+					// 每执行完一段小代码段，都检查一下是否已经超过我们分配的本帧，这些小代码端的最大可执行时间
 					if (new Date().getTime() - startTime > duration) {
+						// 如果超过了，那么本帧就不在执行，开定时器，让下一帧再执行
 						this.scheduleOnce(() => {
 							execute();
 						});
@@ -64,12 +72,14 @@ export default class DirectLoadScrollViewCtrl extends cc.Component {
 					}
 				}
 			};
+
+			// 运行执行函数
 			execute();
 		});
 	}
 
-	private *_getItemGenerator(childNodeCount: number) {
-		for (let i = 0; i < childNodeCount; i++) {
+	private *_getItemGenerator(length: number) {
+		for (let i = 0; i < length; i++) {
 			yield this._initItem(i);
 		}
 	}
